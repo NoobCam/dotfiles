fac62c93cf08cc7bc9102d462fdd2cb985a5d68f
root <root@localhost.localdomain>
2016-08-16 09:55:56 -0500

    first commit

---
+++ rofi.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rofi -show run -terminal rxvt-unicode -lines 5 -eh 2 -width 30 -padding 40 -opacity 90 -bw 0 -font "Mono 24" -color-window "#073642, #002b36, #073642" -color-normal "#073642, #93a1a1, #073642, #073642, #48cfc5" -color-active "#073642, #93a1a1, #073642, #073642, #48cfc5" -color-urgent "#073642, #fc0000, #073642, #073642, #fc0000"
+
+
